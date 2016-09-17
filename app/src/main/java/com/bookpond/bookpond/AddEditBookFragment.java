a0f63c6b8184bdd30715de9b505eff6d3403b5ec
Jun Yamog <jkyamog@gmail.com>
2016-09-18 09:31:08 +1200

    Added AddEditBook fragment, but does not work in fully replacing it.

---
+++ app/src/main/java/com/bookpond/bookpond/AddEditBookFragment.java
@@ -0,0 +1,18 @@
+package com.bookpond.bookpond;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddEditBookFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.add_edit_book, container, false);
+	}
+
+}
--- app/src/main/java/com/bookpond/bookpond/MainActivity.java
+++ app/src/main/java/com/bookpond/bookpond/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bookpond.bookpond;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
+
+				FragmentTransaction ft = getFragmentManager().beginTransaction();
+				ft.replace(R.id.fragment, new AddEditBookFragment());
+
+				ft.addToBackStack("");
+				ft.commit();
+
 				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 						.setAction("Action", null).show();
 			}
