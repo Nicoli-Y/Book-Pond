5263172e0f9ec8da94497e1dc0f20f824bc28e7f
Nicoli Yamog <nicoyamog@gmail.com>
2017-09-26 18:35:03 +1300

    home tab testing with list view

--- app/src/main/java/com/bookpond/bookpond/HomeFragment.java
+++ app/src/main/java/com/bookpond/bookpond/HomeFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class HomeFragment extends Fragment{
@@ -21,7 +23,18 @@ public class HomeFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_home, container, false);
+		View view = inflater.inflate(R.layout.fragment_home, container, false);
+		String[] testItems = {"item 1", "item 2", "item 3"};
+
+		ListView listView= (ListView) view.findViewById((R.id.homeMenu));
+		ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+				getActivity(),
+				android.R.layout.simple_list_item_1,
+				testItems
+		);
+		listView.setAdapter((listViewAdapter));
+
+		return view;
 	}
 
 }
